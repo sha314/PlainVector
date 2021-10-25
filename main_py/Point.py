5f628f1d4f92a5e56a077552e3a9508c432a7c36
sha314 <shahnoor3pl@gmail.com>
2021-10-25 09:15:07 +0600

    coordinate_generate method to generate randomized integer coordinate in any dimension

--- main_py/Point.py
+++ main_py/Point.py
@@ -1,4 +1,6 @@
 import math
+import random
+import utils
 
 class Point:
     def __init__(self, pointA):
@@ -19,3 +21,28 @@ def distance_btn_planes(self, planeA=(0, None, None), planeB=(10, None, None)):
     # planeA and planeB both are in yz plane
 
     pass
+
+
+def coordinate_generate(mins:tuple=(0,0,), maxs:tuple=(20,20,)):
+    # Integer coordinate generation
+    # size of mins and maxs must be equal
+    # their size determins the dimension of space
+
+    xx = []
+    for xmin, xmax in zip(mins, maxs):
+        # xx.append(random.randint(xmin, xmax)*utils.sign())
+        xx.append(random.randint(xmin, xmax) * (lambda: 1 if random.randint(0, 10) % 2 == 0 else -1)())
+        pass
+    return tuple(xx)
+
+
+
+if __name__ == '__main__':
+    print("Generate coordinate")
+    print(coordinate_generate())
+    print(coordinate_generate())
+    print(coordinate_generate())
+    print(coordinate_generate((0, 0, 0), (10, 10, 10)))
+
+
+
